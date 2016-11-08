84ecdf6f7763344e4c44662a546e5c8de84cbabc
shangqiang <1396696447@qq.com>
2016-11-08 19:22:14 +0800

    Signed-off-by: shangqiang <1396696447@qq.com>

--- JavaCore2/src/com/adb/file/XxDemo2.java
+++ JavaCore2/src/com/adb/file/XxDemo2.java
@@ -13,51 +13,45 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.omg.CORBA.INTERNAL;
 
-public class XxDemo {
+public class XxDemo2 {
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		File file=new File("D:"+File.separator+"configuration.txt");
 		//配置文件！
-		config(file);
+		String tmp=config(file);
+		System.out.println(tmp);
 		//设法读取数据 i   j   k?
 		Reader reader=new FileReader(file); 
 		BufferedReader io=new BufferedReader(reader);
-		//解决空指针异常！
-		Integer[] dataArr = null;
-		Scanner sc=null;
-		//sc.useDelimiter(":"+"Math.Integer"+"*");
+		
+		Scanner sc=new Scanner(tmp);
+		sc.useDelimiter(":");
+		sc.useDelimiter("#");
+		List<String> list=new LinkedList<String>();
+		while (sc.hasNext()) {
+			list.add(sc.next());
+		}
+		for (String string : list) {
+			System.out.println(string);
+		}
+		/*Integer[] dataArr=null;
 		int i=0;
-		while(io.readLine()!=null){
-			sc=new Scanner(io.readLine());
-			if (i==3) {
-				/*Scanner m=sc.useDelimiter(":");
-				int n=Integer.parseInt(m.toString());
-				Scanner m2=sc.useDelimiter("#");
-				int n2=Integer.parseInt(m2.toString());
-				Scanner m3=sc.useDelimiter("#");
-				int n3=Integer.parseInt(m3.toString());
-				dataArr[0]=n;
-				dataArr[1]=n2;
-				dataArr[2]=n3;*/
-				sc.useDelimiter(":");
-				sc.useDelimiter("#");
-				sc.useDelimiter("#");
-				while (sc.hasNext()) {
-					//dataArr[i]=Integer.parseInt(sc.next());
-					System.out.println(sc.next()+"  ");
-				}
-			}
-			++i;//不能用i++
+		for (String string : list) {
+			dataArr[i]=Integer.parseInt(string);
+			++i;
 		}
-		System.out.println("获得数据结束！");
-		for (Integer ii : dataArr) {
-			System.out.println(ii);
-		}
+		for (int j = 0; j < dataArr.length; j++) {
+			System.out.println(dataArr[j]);
+		}*/
+		System.out.println("获得数据结束!");
+		 
 		//将解题思路写入到配置文件？
 		//将计算得到的结果写入   有多个结果换行输出，以方便阅读？（可以在遍历数组 write 然后writer.newline();
 		///读取满足条件的数据
@@ -82,7 +76,7 @@ public class XxDemo {
 
 		
 	}
-	public static void config(File file) throws IOException{
+	public static String config(File file) throws IOException{
 		OutputStream os=new FileOutputStream(file, true);
 		Writer writer=new OutputStreamWriter(os,"UTF-8");
 		//Writer writer=new FileWriter(file,true);
@@ -90,14 +84,16 @@ public class XxDemo {
 		int m=Integer.parseInt(sc.next());
 		int m2=Integer.parseInt(sc.next());
 		int m3=Integer.parseInt(sc.next());
-		writer.write("读取当前行的内容是：这是一个java上机题的文档文件：\r\n");
-		writer.write("读取当前行的内容是：一个正整数,它加上"+m+"后是一个完全平方数,再加上"+m2+"又是一个完全平方数,请问该数是多少？\r\n");
-		writer.write("读取当前行的内容是：需要的参数如下：通过解析下面的字母串得到需要的参数值\r\n");
-		writer.write("读取当前行的内容是："+m+"#"+m2+"#"+m3+"\r\n");
-		writer.write("当前行中，有需要的参数，参数为："+m+"   "+m2+"   "+m3+"\r\n");
-		writer.write("不再读取当前文件，读取配置文件的方法退出！\r\n");
+		writer.write("读取当前行的内容是:这是一个java上机题的文档文件:\r\n");
+		writer.write("读取当前行的内容是:一个正整数,它加上"+m+"后是一个完全平方数,再加上"+m2+"又是一个完全平方数,请问该数是多少？\r\n");
+		writer.write("读取当前行的内容是:需要的参数如下：通过解析下面的字母串得到需要的参数值\r\n");
+		String string4="读取当前行的内容是:"+m+"#"+m2+"#"+m3+"\r\n";
+		writer.write(string4);
+		writer.write("当前行中，有需要的参数，参数为:"+m+"   "+m2+"   "+m3+"\r\n");
+		writer.write("不再读取当前文件，读取配置文件的方法退出!\r\n");
 		writer.close();
 		System.out.println("初始文件写入完毕！\r\n");
+		return string4;
 	}
 
 }
